bb6ef23ee90c5db0bb492585215a8ca445a2925a
Aitor <aitor@rojo2.com>
2016-07-06 17:26:04 +0200

    Change MAX_QUEUE_SIZE to 16384

--- src/impl/channels.js
+++ src/impl/channels.js
@@ -4,7 +4,7 @@ var buffers = require("./buffers");
 var dispatch = require("./dispatch");
 
 var MAX_DIRTY = 64;
-var MAX_QUEUE_SIZE = 1024;
+var MAX_QUEUE_SIZE = 16384; // 1024 * 16
 
 var CLOSED = null;
 
